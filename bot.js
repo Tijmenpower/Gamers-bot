6922401ebcdfb77cdefb98507c9617b263c65f04
Tijmenpower <50448591+Tijmenpower@users.noreply.github.com>
2019-05-09 14:04:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.faceeee.1352}!`);
 });
 
 client.on('message', msg => {
@@ -11,5 +11,5 @@ client.on('message', msg => {
   }
 });
 
-// ThIS MUST BE THIS WAY
+// THIS MUST BE THIS WAY
 client.login('8aV_34Nrz5ZZRngexYAMCzXriA2UvO_n');
